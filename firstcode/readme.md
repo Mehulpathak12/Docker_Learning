b701f190a569f2d15db08e1d582095cd9e563399
Mehulpathak12 <mehulpathak48@gmail.com>
2025-06-26 14:29:33 +0530

    Initial Commit

---
+++ firstcode/readme.md
@@ -0,0 +1,1 @@
+#This is the simple code for executing java code in docker container
---
+++ firstcode/src/Main.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hey this is the sample code for the docker! and today date is " + currentDate);
+    }
+}
